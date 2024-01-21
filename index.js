2a799164ee5003181eff8c70be8d5ca65ca13dcf
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-21 22:04:22 +0300

    Context

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
-/* Your Code Here */
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ');
+    this.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour) });
+    return this;
+}
+
+function createTimeOutEvent(dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ');
+    this.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour) });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce(function (totalPay, employee) {
+        return totalPay + allWagesFor.call(employee);
+    }, 0);
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
